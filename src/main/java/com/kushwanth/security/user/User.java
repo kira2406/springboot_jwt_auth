aa74b02050b3c7cf65d8f374346141b0f29210e3
Kushwanth P <Kushwanth.P@Cognizant.com>
2023-02-03 01:35:26 +0530

    Initial commit

---
+++ src/main/java/com/kushwanth/security/user/User.java
@@ -0,0 +1,25 @@
+package com.kushwanth.security.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
